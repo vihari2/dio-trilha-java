66098a548352f5ee3685771f1128676c74d17759
Vitoria <freitasvitoria712@gmail.com>
2023-05-23 23:00:16 -0300

    add files

---
+++ src/edu/vitoria/primeirasemana/Nome.java
@@ -0,0 +1,14 @@
+package edu.vitoria.primeirasemana;
+
+public class Nome {
+
+    public static String nomeCompleto(String nome, String sobrenome) {
+        return nome + " " + sobrenome;
+    }
+    public static void main(String[] args) {
+        String nome = "Vitoria";
+        String sobrenome = "Freitas";
+        String meuNome = nomeCompleto(nome, sobrenome);
+        System.out.println(meuNome);
+    }
+}
