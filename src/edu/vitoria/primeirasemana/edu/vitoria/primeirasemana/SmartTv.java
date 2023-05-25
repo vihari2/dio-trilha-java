b28bd526cc832aeb2ff15304d7bd7c690ac36743
Vitoria <freitasvitoria712@gmail.com>
2023-05-25 00:10:49 -0300

    [add] ex

---
+++ src/edu/vitoria/primeirasemana/edu/vitoria/primeirasemana/SmartTv.java
@@ -0,0 +1,38 @@
+package edu.vitoria.primeirasemana;
+
+public class SmartTv {
+    //atributos 
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 24; 
+    //metodo
+    public void ligar() {
+        ligada = true;
+    }
+    //metodo
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+
+    }
+
+    public void aumentarVolume() {
+        volume++;
+
+    }
+}
